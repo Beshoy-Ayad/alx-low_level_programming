4faefd850f5b5ee155d6cd24af633d2ecd247005
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-16 06:20:37 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main (void)
+/*main Function*/
+int main(void)
 {
 /* code */
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
