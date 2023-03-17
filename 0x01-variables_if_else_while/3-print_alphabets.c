88d18250e39e234f4944bb060d89498bdc06d227
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-17 11:55:16 -0700

    3.1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	char d;
+
+	for (d = 'A' ; d <= 'Z' ; d++)
+	{
+		putchar(d);
+	}
+	putchar ('\n');
+	return (0);
+}
