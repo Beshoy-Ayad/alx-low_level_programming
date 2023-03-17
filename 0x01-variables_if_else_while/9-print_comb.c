338082ea524028db4b7bffef9e393db6ee625fb9
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-17 15:15:48 -0700

    9.4

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,24 +8,16 @@
 int main(void)
 {
 	int i;
-	int j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		putchar(i);
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-			putchar(j + '0');
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
