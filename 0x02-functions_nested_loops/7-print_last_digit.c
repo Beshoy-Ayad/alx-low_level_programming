c60607bc8665c78174f12cf3dbaee3822b8b578a
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 15:59:05 -0700

    7.6

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,23 @@
  * @n: print letter
  * Return:  last digit
  */
+
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
-		n *= -1;
+	{
+		n = -n;
+	}
 
-	_putchar('0' + (n % 10));
+	last_digit = n % 10;
 
-	return (n % 10);
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
