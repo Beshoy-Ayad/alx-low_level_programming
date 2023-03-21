7ee810b4a4746887b4d138d4fa2c4779250ae72e
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 15:27:58 -0700

    6.1

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _abs  - Entry pointi
+ * @c: print letter
+ * Return:  1 (Letter), 0 (Otherwise)
+ */
+
+int int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c)
+	}
+}
