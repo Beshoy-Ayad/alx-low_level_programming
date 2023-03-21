6f7e0174d3613e875f89f9b6dae5e69529e7e811
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 09:31:27 -0700

    2.1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  print_alphabet_x10 - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int c = 'a';
+	int d = 1;
+
+	while (d <= 10)
+	{
+		while (c <= 'z')
+		{
+			puchar(c);
+		}
+	}
+	putchar('\n');
+}
+
