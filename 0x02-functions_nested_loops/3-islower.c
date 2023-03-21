16d161b123fa2477b09e4a2fab0f3fa2c93927ab
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 13:53:32 -0700

    3.2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * _islower  - Entry point
- * Return:  1 (lower)
- * Return:  2 (upper)
+ * _putchar - Entry point
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _islower(int c)
+int _putchar(char c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	return (write(1, &c, 1));
+}
+
+int main(void)
+{
+	int r;
+
+	r = 0;
+	while (r < 10)
+	{
+		_putchar(r + '0');
+		r++;
+	}
+	_putchar('\n');
+	return (0);
 }
