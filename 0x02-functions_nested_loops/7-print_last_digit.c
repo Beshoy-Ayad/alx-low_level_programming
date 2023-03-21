3298ec520d27212f3ce92bafda271021089ef744
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 15:47:35 -0700

    7.4

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isalpha - Entry pointi
+ * print_last_digit - Entry pointi
  * @n: print letter
  * Return:  last digit
  */
@@ -8,14 +8,14 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	
+
 	if (n < 0)
 	{
 		n *= -1;
 	}
 
-	last_digit = n % 10;
-	
-	_putchar(last_digit + '0');
+	last_digit = (n % 10);
+
+	_putchar('0' +  last_digit);
 	return (last_digit);
 }
