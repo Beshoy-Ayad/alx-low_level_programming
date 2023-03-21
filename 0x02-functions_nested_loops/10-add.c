4a6614c0567eee903a751dae124dcbb376a3f503
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 16:18:13 -0700

    10.1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: 1st integer arguments
+ * @b: 2nd integer arguments
+ * Return: 1st + 2nd
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
