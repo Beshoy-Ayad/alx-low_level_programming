1637f61de0d807a1eb73b68e2a6a22bf1ea07d3c
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 15:12:40 -0700

    5.2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,12 +13,12 @@ int print_sign(int n)
 		putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	else if(n == 0)
 	{
 		putchar(0);
 		return (0);
 	}
-	if (n < 0)
+	else (n < 0)
 	{
 		putchar('-');
 		return (-1);
