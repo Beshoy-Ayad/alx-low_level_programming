3d49414ca05b64bbbc907896a573d9bcee522ecb
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 15:36:15 -0700

    7.1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Entry pointi
+ * @n: print letter
+ * Return:  last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit;
+	
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	last_digit = n % 10;
+	
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
