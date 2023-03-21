0477a4249dd04d2f1767a790524f8ed784fd0abf
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-21 08:40:45 -0700

    1.3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,13 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet (void)
 {
 	char c = 'a';
 
