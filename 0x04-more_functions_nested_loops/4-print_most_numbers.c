7dc13c38551ee9eacfc3a5cde589ef6233bdb198
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 11:49:15 -0700

    4.1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ * Return:  1 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for ( i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n')
+}
