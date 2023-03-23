bb00d231436889a191836c4cdccbde317be865a8
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 13:22:30 -0700

    6.1

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+}
