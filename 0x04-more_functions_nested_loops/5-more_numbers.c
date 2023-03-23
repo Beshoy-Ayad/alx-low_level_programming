fa1ee14f7bd4e6dc4a829c75a29f60138f23045d
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 13:09:00 -0700

    5.8

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 	int i;
 	int n;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 1 ; i <= 10 ; i++)
 	{
 		for (n = 0 ; n <= 14 ; n++)
 		{
 			if (n > 9)
-				_putchar(1 + 48);
-			_putchar((n % 10) + 48);
+				_putchar(1 + '0');
+			_putchar((n % 10) + '0');
 		}
 		 _putchar('\n');
 	}
