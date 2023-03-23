8348db82c99de384c17099dfd0ec2c38a34dc35e
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 14:43:41 -0700

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - printint
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	unsigned int t;
+	unsigned int d;
+	unsigned int p = n;
+	double tt = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			positive = n * -1;
+			_putchar('-');
+		}
+
+		while (tt <= p)
+			tt *= 10;
+		t = tt / 10;
+
+		while (t >= 1)
+		{
+			d = p / t;
+			_putchar(d + '0');
+			p = (p - (t * d));
+			t /= 10;
+		}
+	}
+}
