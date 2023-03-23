8c9ef2cc62a7962a82f288bb9acc70415932d392
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 11:26:38 -0700

    1.0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ * @c: print letter
+ * Return:  1 (digit), 0 (otherwise)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
