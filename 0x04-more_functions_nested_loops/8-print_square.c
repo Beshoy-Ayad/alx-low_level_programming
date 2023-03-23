d36d22f00bdabe320411b2231661933b8bf92195
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 14:05:34 -0700

    8.5

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,17 +11,18 @@ void print_square(int size)
 	int i;
 	int j;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-		return;
-	}
-	for (i = 0 ; i < size ; i++)
-	{
-		for (j = 0 ; j <= size ; j++)
+		for (i = 0 ; i < size ; i++)
 		{
-		_putchar('#');
+			for (j = 0 ; j <= size ; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+
 	}
+	else
+		_putchar('\n');
 }
