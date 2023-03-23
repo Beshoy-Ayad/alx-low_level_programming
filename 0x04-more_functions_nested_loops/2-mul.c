60cc2010abd6ae1dc2f38c7535cb800add45af66
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 11:34:47 -0700

    2.1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  mul - Entry pointi
+ * @a: first int
+ * @b: second int
+ * Return:  1 (Lower), 0 (Upper)
+ */
+
+int mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+	return (i);
+}
