58c2ae4b3bdaf83b30e08d6bd4ba02804287eee5
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 12:18:36 -0700

    5.1

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - Entry pointi
+ * @c: print letter
+ * Return:  1 (Lower), 0 (Upper)
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int n;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (n = 0 ; n <= 14 ; n++)
+		{
+			_putchar(n + '0');
+		}
+		_putchar('\n');
+	}
+}
