0bca7e5d67647a2f36e52d9514abe1a9be80bd4f
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 11:40:40 -0700

    3.1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Entry pointi
+ * Return:  1 (success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
