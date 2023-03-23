81de37080fe65065c14e687d5aafab00b84e5068
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-23 13:59:14 -0700

    8.4

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,9 +16,9 @@ void print_square(int size)
 		_putchar('\n');
 		return;
 	}
-	for (i = 1 ; i < size ; i++)
+	for (i = 0 ; i < size ; i++)
 	{
-		for (j = 1 ; j <= i ; j++)
+		for (j = 0 ; j <= size ; j++)
 		{
 		_putchar('#');
 		}
