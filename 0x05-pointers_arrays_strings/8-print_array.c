2ee1aeccafd6dd9ddb8326e479ac8d0d39f0310f
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-27 13:38:00 -0700

    8.1

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: string
+ * @n: string
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
