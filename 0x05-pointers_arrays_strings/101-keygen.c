9b39a692f57f2ff9311bf91466cffba4c96befcc
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-27 14:31:54 -0700

    101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+/**
+ * main - generates random valid passwords for the program 101-crackme
+ */
+
+int main(void)
+{
+	char *chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int len = 64;
+	int sum = 2772;
+	int i, val;
+	char pass[len + 1];
+
+	srand(time(NULL));
+
+	for (i = 0; i < len - 1; i++)
+	{
+		val = rand() % (sum / (len - i));
+		pass[i] = chars[val % 62];
+		sum -= val;
+	}
+	pass[i] = sum;
+	pass[i + 1] = '\0';
+	printf("%s\n", pass);
+	return (0);
+}
