46e38b61bedcfdcf1454cd077a0a9df09d39e26e
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-03-29 13:51:04 -0700

    6.1

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ *
+ * Return: result
+ */
+
+char *cap_string(char *s)
+{
+	char *p = s;
+	int flag = 1;
+
+	while (*p)
+	{
+		if (*p >= 'a' && *p <= 'z')
+		{
+			if (flag)
+		{
+			*p = *p - 32;
+			flag = 0;
+		}
+	}
+	else if (*p == ' ' || *p == '\t' || *p == '\n' || *p == ','
+|| *p == ';' || *p == '.' || *p == '!' || *p == '?' || *p == '"'
+|| *p == '(' || *p == ')' || *p == '{' || *p == '}')
+	{
+		flag = 1;
+	}
+		p++;
+	}
+
+	return (s);
+}
