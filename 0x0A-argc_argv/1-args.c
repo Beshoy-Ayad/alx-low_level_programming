9eb758491eb1e1ecfc5893231e77b82ee1057085
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-10 07:22:31 -0700

    1.0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+
+ int main (int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return 0;
+}
