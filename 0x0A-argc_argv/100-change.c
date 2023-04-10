23dbb1f27f87530130dc3bdfab12e92cbd37f2b6
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-10 08:14:39 -0700

    100.2

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins[] = {25, 10, 5, 2, 1};
+	int n_coins = 0;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		if (cents >= coins[i])
+		{
+			cents -= coins[i];
+			n_coins++;
+		}
+	}
+
+	printf("%d\n", n_coins);
+	return (0);
+}
