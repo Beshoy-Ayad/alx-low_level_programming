9d24d6afd3ab64ce239ab9a293e22efa29dc3da2
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-11 08:28:38 -0700

    101.5

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,38 +11,59 @@
 char **strtow(char *str)
 {
 	char **words;
-	int i, j, len = 0, word_count = 0;
+	int i, j, k, len, count, start, end;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	count = 0;
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
-			word_count++;
+		while (str[i] == ' ')
+			i++;
+		if (str[i] != '\0')
+		{
+			count++;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+		}
 	}
-	words = malloc((word_count + 1) * sizeof(char *));
+	if (count == 0)
+		return (NULL);
+	words = malloc(sizeof(char *) * (count + 1));
 	if (words == NULL)
 		return (NULL);
-	for (i = 0; i < word_count; i++)
+	i = 0;
+	k = 0;
+	while (str[i] != '\0' && k < count)
 	{
-		while (*str == ' ')
-			str++;
-		for (j = 0; str[j] != ' ' && str[j] != '\0'; j++)
-			len++;
-		words[i] = malloc((len + 1) * sizeof(char));
-		if (words[i] == NULL)
+		while (str[i] == ' ')
+			i++;
+		if (str[i] != '\0')
 		{
-			for (; i >= 0; i--)
-				free(words[i]);
-			free(words);
-			return (NULL);
+			len = 0;
+			start = i;
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				len++;
+				i++;
+			}
+			end = i - 1;
+			words[k] = malloc(sizeof(char) * (len + 1));
+			if (words[k] == NULL)
+			{
+				for (j = 0; j < k; j++)
+					free(words[j]);
+				free(words);
+				return (NULL);
+			}
+			for (j = start; j <= end; j++)
+				words[k][j - start] = str[j];
+			words[k][len] = '\0';
+			k++;
 		}
-		strncpy(words[i], str, len);
-		words[i][len] = '\0';
-		str += len;
-		len = 0;
 	}
-	words[word_count] = NULL;
+	words[k] = NULL;
 	return (words);
 }
+
