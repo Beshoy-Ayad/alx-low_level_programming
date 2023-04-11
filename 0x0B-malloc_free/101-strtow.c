b01dabe9dd14f4eec3a862f58eb67ab7db2d4e80
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-11 08:14:32 -0700

    101.3

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,48 @@
+#include " main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (words) or NULL if it fails
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i, j, k = 0, len = 0, word_count = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			word_count++;
+	}
+	words = malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	for (i = 0; i < word_count; i++)
+	{
+		while (*str == ' ')
+			str++;
+		for (j = 0; str[j] != ' ' && str[j] != '\0'; j++)
+			len++;
+		words[i] = malloc((len + 1) * sizeof(char));
+		if (words[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(words[i]);
+			free(words);
+			return (NULL);
+		}
+		strncpy(words[i], str, len);
+		words[i][len] = '\0';
+		str += len;
+		len = 0;
+	}
+	words[word_count] = NULL;
+	return (words);
+}
