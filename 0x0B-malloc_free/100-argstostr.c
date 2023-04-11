44c2f1974187675f1a13a1d59fc6804d9c1d58a3
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-11 08:08:57 -0700

    100.3

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,25 +11,39 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *concat_str;
-	int i, j, k = 0, len = 0;
+	char *str;
+	int i, j, k, len;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		if (av[i] == NULL)
-			return (NULL);
-		len += strlen(av[i]) + 1;
-	}
-	concat_str = malloc(len * sizeof(char));
-	if (concat_str == NULL)
-		return (NULL);
+
+	len = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-			concat_str[k++] = av[i][j];
-		concat_str[k++] = '\n';
+		{
+			len++;
+		}
+		len++;
 	}
-	return (concat_str);
+	len++;
+
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
 }
