54894d118344e2f3b2f8a672627b019174c18433
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-19 07:51:46 -0700

    0.1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name
+ * @f: pointer
+ * Return: name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return (f(name));
+	}
+}
