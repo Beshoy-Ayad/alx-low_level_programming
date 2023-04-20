97ea83d7c2dda4740f1bbf5d3857d5678af940e3
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-20 07:40:39 -0700

    100.2

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,15 +9,18 @@
  * Return: 0 on success, 1 on incorrect number of arguments,
  * 2 if number of bytes is negative
  */
-int main(int argc, char **argv)
+
+int main(int argc, char *argv[])
 {
-	int i, bytes;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
 
 	if (bytes < 0)
@@ -25,11 +28,17 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(2);
 	}
+
+	arr = (char *)main;
+
 	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x ", ((unsigned char *)main)[i]);
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
 	}
-	printf("\n");
-
 	return (0);
 }
