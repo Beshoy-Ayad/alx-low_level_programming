98bef17fdcf0ab9ab0930de03517c92a20421d0a
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-27 13:15:13 -0700

    0.4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,23 +8,21 @@
 
 size_t print_list(const list_t *h)
 {
-        size_t num_nodes_printed = 0;
+	size_t num_nodes_printed = 0;
 
-        while (h != NULL)
-        {
-                if (h->str == NULL)
-                {
-                        printf("[0] (nil)\n");
-                }
-                else
-                {
-                        printf("[%u] %s\n", h->len, h->str);
-                }
-                h = h->next;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
 
-                num_nodes_printed++;
-        }
-
-        return (num_nodes_printed);
+		num_nodes_printed++;
+	}
+	return (num_nodes_printed);
 }
-
