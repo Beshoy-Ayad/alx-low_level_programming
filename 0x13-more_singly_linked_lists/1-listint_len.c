df26679d2170bce3c39a02d04d5e6deb4710e404
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-30 06:01:53 -0700

    1.1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+/**
+ * listint_len - returns the number of elements in a listint_t list
+ * @h: pointer to the listint_t list
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
