bfb90668436039369b696f3e1342509f58a4c3cc
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-04-30 05:40:32 -0700

    fixing betty lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,8 +16,8 @@ int _putchar(char c);
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
