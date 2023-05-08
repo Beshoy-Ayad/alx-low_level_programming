a47d13dbe59e1a96e4b58a93f8c1bdb6fd4a6f0d
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-05-08 13:32:43 -0700

    1.2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 		}
 		nwrite = write(fd, text_content, len);
-		if (nwrite == -1 || nwrite != len)
+		if (nwrite == -1 ||(size_t)nwrite != len)
 		{
 			close(fd);
 			return (-1);
