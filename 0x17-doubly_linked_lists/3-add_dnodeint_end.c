2f3fa615e9020af05a893480f0c782afc308fff8
Beshoy Ayad <Beshoy.Ayad@gmail.com>
2023-06-15 14:20:38 -0700

    3.3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new, *last;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -22,7 +22,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new->prev = NULL;
 		*head = new;
-		*tail = new;
 		return (new);
 	}
 
